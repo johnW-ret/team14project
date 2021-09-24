55d089d370c65db6ff557c8b206d19910448a559
johnW_ret <retruate@gmail.com>
2021-09-24 16:06:01 -0500

    Defined script assemblies and example tests.
    - Set a root namespace.
    - Set folder structure for scripts and defined assembly definitions.
    - Defined example tests.

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TeamFourteen
+{
+    public class ExampleScript : MonoBehaviour
+    {
+        private readonly string helloWorldText = "Hello World!";
+        [ContextMenu("Print Hello World")]
+        public string PrintHelloWorld()
+        {
+            Debug.Log(helloWorldText);
+            return helloWorldText;
+        }
+
+        public static int AddIntegers(int a, int b) => a + b;
+    }
+}
---
+++ Assets/Tests/TeamFourteen/ExampleTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using TeamFourteen;
+
+namespace Tests
+{
+    public class Example
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ExampleSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual(ExampleScript.AddIntegers(5, 6), 11);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ExampleWithEnumeratorPasses()
+        {
+            ExampleScript exampleScript = new GameObject().AddComponent<ExampleScript>();
+
+            Assert.AreEqual(exampleScript.PrintHelloWorld(), "Hello World!");
+
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
