9386b5acede497b0a0e27cf3013f4e3400776bf9
Boyd-Michael <64430317+Boyd-Michael@users.noreply.github.com>
2021-11-04 22:22:12 -0500

    Pedestals/LanternInteraction
    Created two scripts for the interaction between a Pedestal and a Lantern. The other two variables that are need on Lantern or the Pedestal is a Rigidbody and a Collider that is Triggerable.

---
+++ Assets/Scripts/Colliders/LanternPlacement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternPlacement : MonoBehaviour
+{
+    //public bool lanternTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If Lantern is Triggered by the PlatformCollider
+        if(other.gameObject.GetComponent<PlatformCollider>())
+        {
+            other.gameObject.GetComponent<PlatformCollider>().platformState = true;
+
+            //Score Update
+            if (!other.gameObject.GetComponent<PlatformCollider>().scoreState)
+            {
+                //Increase Score
+
+                //Doesn't allow the score to be increased by this platform
+                other.gameObject.GetComponent<PlatformCollider>().scoreState = true;
+            }
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //If Lantern is Triggered by the PlatformCollider
+        if (other.gameObject.GetComponent<PlatformCollider>())
+        {
+            //If the platform has multiple use
+            if(!other.gameObject.GetComponent<PlatformCollider>().oneTimeUse)
+            {
+                other.gameObject.GetComponent<PlatformCollider>().platformState = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Colliders/PlatformCollider.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCollider : MonoBehaviour
+{
+    public bool platformState;
+    public bool scoreState;
+    public bool oneTimeUse = true;
+
+    private bool Usable;
+
+    private void Start()
+    {
+        platformState = false;
+        scoreState = false;
+        Usable = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //This Platform is only usable once
+        if (oneTimeUse)
+        {
+            //If it is one time use, has it been used
+            if (Usable)
+            {
+                if (platformState == true)
+                {
+                    //Interacts with level
+                }
+                //While platformStat is false
+                else
+                {
+                    //Interacts with level
+                }
+                Usable = false;
+            }
+            else
+            {
+                if (platformState == true)
+                {
+                    //Interacts with level
+                }
+                //While platformState is false
+                else
+                {
+                    //Interacts with level
+                }
+            }
+        }
+        //If the Platform has more usage
+        else
+        {
+            if(platformState == true)
+            {
+                //Interacts with level
+            }
+            //While false what would it do
+            else
+            {
+                //Interacts with level
+            }
+        }
+    }
+}
