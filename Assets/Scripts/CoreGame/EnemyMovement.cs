9f3877f991e9cb80b3fb7a0d3ac2e9be1e59a07d
johnW_ret <retruate@gmail.com>
2021-11-19 15:42:43 -0600

    Defined example Enemy prefab and behaviour
    Defined Enemy prefab as variant of Actor. Created "EnemyMovement" script with target.

---
+++ Assets/Scripts/CoreGame/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] [HideInInspector] private NavMeshAgent nmAgent;
+
+    private void Reset()
+    {
+        SetRefernces();
+    }
+
+    [ContextMenu("Set References")]
+    private void SetRefernces()
+    {
+        if (!nmAgent)
+            nmAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (target)
+            nmAgent.destination = target.position;
+    }
+}
