a981156f4be75eab8397941d873e27d08ac96ca9
johnW_ret <retruate@gmail.com>
2021-11-20 13:32:47 -0600

    Added selection handler and container

---
+++ Assets/Scripts/CoreGame/ISelectionHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamFourteen.CoreGame
+{
+    /// <summary>
+    /// Represents an object that can execute selection event methods of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of object that is being selected.</typeparam>
+    public interface ISelectionHandler<T>
+    {
+        OnSelectEvent<T> OnSelect { get; }
+        OnSelectEvent<T> OnDeselect { get; }
+    }
+}
---
+++ Assets/Scripts/CoreGame/ISelectionHandlerContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamFourteen.CoreGame
+{
+    /// <summary>
+    /// Represents a container that holds references to <see cref="ISelectionHandler{T}"/> and calls <see cref="OnSelectEvent{T}"/> events.
+    /// </summary>
+    /// <typeparam name="T">Type of object that is being selected.</typeparam>
+    public interface ISelectionHandlerContainer<T>
+    {
+        bool Subscribe(ISelectionHandler<T> handler);
+        
+        // maybe Unsubcribe(ISelectionHandler<T> handler) ??? lol
+        // would need dictionary maybe idk. not really needed now.
+    }
+}
