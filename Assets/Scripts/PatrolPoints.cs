4090bfb14d895ee776335d433fd2d7a7c3c53dba
Boyd-Michael <64430317+Boyd-Michael@users.noreply.github.com>
2021-11-20 17:34:04 -0600

    Patrol Points Setup
    Currently the Enemy Agent can travel between two or more points given to it through a list.

---
+++ Assets/Scripts/PatrolPoints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PatrolPoints : MonoBehaviour
+{
+    //Adjust the radius of the Gizmos of the given Game Object
+    [SerializeField] float PointRadius = 0.5f;
+
+    public virtual void OnDrawGizmos()
+    {
+        //Color and draws a sphere at the location of the Game Object
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, PointRadius);
+    }
+}
