10ed94aaff4ab9e49e22bbd586846a9b26b07969
johnW_ret <retruate@gmail.com>
2021-11-30 14:38:40 -0600

    Renamed and moved PatrolPoints
    - Renamed PatrolPoints to PatrolPoint
    - Moved to namespace TeamFourteen.CoreGame

---
+++ Assets/Scripts/CoreGame/PatrolPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TeamFourteen.CoreGame
+{
+    public class PatrolPoint : MonoBehaviour
+    {
+        //Adjust the radius of the Gizmos of the given Game Object
+        [SerializeField] private float PointRadius = 0.5f;
+
+        public virtual void OnDrawGizmos()
+        {
+            //Color and draws a sphere at the location of the Game Object
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, PointRadius);
+        }
+    }
+}
